d0ef6037ff068988ddbc3e069c30a8450292d61a
Maxence Ahlouche <maxence.ahlouche@gmail.com>
2012-12-07 14:56:17 +0100

    Added files for easy testing through interpretor

---
+++ src/init.ml
@@ -0,0 +1,7 @@
+open Logo_types;;
+open Logo_analyseur_lexical;;
+open Logo_analyseur_syntaxique;;
+open Logo_analyseur_semantique;;
+
+let lmot = analyseur_lexical "../EXEMPLES/test.logo";;
+let prog = analyseur_syntaxique lmot;;
