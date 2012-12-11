cb512600d54344e3fe4355a9b095b0b3c4b0fe31
Maxence Ahlouche <maxence.ahlouche@gmail.com>
2012-12-11 11:54:24 +0100

    Implementation of function evalue_expression

--- src/logo_analyseur_semantique.ml
+++ src/logo_analyseur_semantique.ml
@@ -1,5 +1,14 @@
-let evalue_expression environnement expr =
-	failwith "A Faire";;
+let evalue_expression env expr =
+  match expr with
+  | Const c -> c
+  | Plus(a, b) -> (evalue_expression env a) +. (evalue_expression env b)
+  | Moins(a, b) -> (evalue_expression env a) -. (evalue_expression env b)
+  | Div(a, b) -> (evalue_expression env a) /. (evalue_expression env b)
+  | Mult(a, b) -> (evalue_expression env a) *. (evalue_expression env b)
+  | Cosinus(t) -> cos (evalue_expression env t)
+  | Sinus(t) -> sin (evalue_expression env t)
+  | Tangente(t) -> tan (evalue_expression env t)
+  | Var(v) -> failwith "TODO";;
 
 let evalue_condition environnement test = 
 	failwith "A Faire";;
