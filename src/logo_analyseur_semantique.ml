db2bba5ce8796cddb3c85488ca66e82aff43b8ed
Maxence Ahlouche <maxence.ahlouche@gmail.com>
2012-12-11 20:25:18 +0100

    Implemented function eval_instructions, still have to implement Call function and defs

--- src/logo_analyseur_semantique.ml
+++ src/logo_analyseur_semantique.ml
@@ -20,8 +20,40 @@ let rec evalue_condition env test =
   | Or(a,b) -> ((evalue_condition env a) || (evalue_condition env b))
   | Not(a) -> not (evalue_condition env a);;
 
-let execute_instruction environnement instruction etat =
+let execute_instruction env instruction etat =
 	failwith "A Faire";;
 
-let execute_programme programme =
-	failwith "A Faire";;
+let etat_initial = ((0., 0.), 0., Graphics.black);;
+let env_initial = [];;
+
+let execute_programme (defs, instructions) =
+  let rec eval_defs defs env =
+    failwith "TODO: eval_defs"
+  in
+  let rec eval_instructions instructions env etat =
+    match instructions with
+    | [] -> []
+    | Move(e)::instructions'   -> let (cmd, _, nouvel_etat) = (execute_instruction env (Move(e)) etat) in
+                                    cmd::(eval_instructions instructions' env nouvel_etat)
+    | Jump(e)::instructions'   -> let (cmd, _, nouvel_etat) = (execute_instruction env (Jump(e)) etat) in
+                                    cmd::(eval_instructions instructions' env nouvel_etat)
+    | Rotate(e)::instructions' -> let (_, _, nouvel_etat) = (execute_instruction env (Rotate(e)) etat) in
+                                    (eval_instructions instructions' env nouvel_etat)
+    | Color(c)::instructions'  -> let (cmd, _, nouvel_etat) = (execute_instruction env (Color(c)) etat) in
+                                    cmd::(eval_instructions instructions' env nouvel_etat)
+    | If(test, instructions_if, instructions_else)::instructions' 
+                               -> if evalue_condition env test then 
+                                    eval_instructions (instructions_if@instructions') env etat
+                                  else
+                                    eval_instructions (instructions_else@instructions') env etat
+    | Repeat(expr, instructions_rpt)::instructions'
+                               -> let x = evalue_expression env expr in 
+                                    if x > 0. then
+                                      eval_instructions ((Repeat(Const(x-.1.), instructions_rpt))::(instructions_rpt@instructions')) env etat
+                                    else
+                                      eval_instructions instructions' env etat
+    | Call::instructions' -> failwith "TODO: eval_instructions->Call"
+
+
+
+  in let env = eval_defs defs env_initial in eval_instructions instructions env etat_initial;;
