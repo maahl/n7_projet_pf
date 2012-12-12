7f033d1b09595f671a6e731a01a8a0ba90fe376e
Maxence Ahlouche <maxence.ahlouche@gmail.com>
2012-12-12 01:26:05 +0100

    Installed ledit for more comfort during tests

--- src/test.sh
+++ src/test.sh
@@ -1,4 +1,4 @@
 #! /bin/zsh
 
-ocaml graphics.cma logo_lexer.cmo logo_parser.cmo logo_analyseur_lexical.cmo logo_analyseur_syntaxique.cmo logo_analyseur_semantique.cmo logo_dessine.cmo
+ledit ocaml graphics.cma logo_lexer.cmo logo_parser.cmo logo_analyseur_lexical.cmo logo_analyseur_syntaxique.cmo logo_analyseur_semantique.cmo logo_dessine.cmo
 logo_main.cmo
